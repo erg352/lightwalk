4f1c4aa57cde8a4107725db54bf3348207a4e3c3
emile <emile@guyotfamily.com>
2025-05-02 07:27:43 +0200

    Updated the hello_sdf example

--- examples/hello_sdf.rs
+++ examples/hello_sdf.rs
@@ -10,15 +10,23 @@ fn main() {
     // 'scope' of the SDF. As an example, we are translating by a 2D vector, and as such, the SDF
     // will be in 2D. If we translated with a 3D Vector, we would have had a 3D SDF and so on.
     let sphere = Sphere.scale(4.).translate(&[1., 2.]);
-    let cube = Cube.scale(2.).rotate_2d(PI / 4.).translate(&[-3., 4.]);
+    let cube = Cube
+        .scale(2.)
+        .round(0.3)
+        .rotate_2d(PI / 4.)
+        .translate(&[-3., 4.]);
+
+    // SDFs can be created from primitives without thickness, but it may be wise to specify a
+    // thickness!
+    let line = line([1., 1.]).thickness(0.4);
 
     // We can compose multiple SDFs together so long as they are in the same dimension (here, they
     // are both 2D) and have the same scalar type. (Or float type. Here, both are f32s.)
-    let union = sphere.add(cube);
+    let combined = sphere.add(cube).add(line);
 
     // We can sample the SDF at a point in space by calling the distance function (or others, like
     // distance_ref) to get the distance at said point to the shape we described earlier.
-    let distance_to_scene = union.distance([-2., -2.]);
+    let distance_to_scene = combined.distance([-2., -2.]);
 
     println!("Distance to scene: {distance_to_scene}");
 }
